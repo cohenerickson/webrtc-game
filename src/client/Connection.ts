21563b530db3f9dd0ca5ac16cca018ff8487a808
Cohen Erickson <cohenerickson@gmail.com>
2022-12-19 23:10:04 -0600

    debug

--- src/client/Connection.ts
+++ src/client/Connection.ts
@@ -19,38 +19,95 @@ export default class Connection extends EventEmitter {
         ids.forEach((id): void => {
           if (id === this.#self.id) return;
           if (this.#peers.has(id)) return;
-          this.subscribePeer(id, this.#self.connect(id, { reliable: true }));
+          const peerConnection = this.#self.connect(id, { reliable: true });
+          this.#subscribePeer(id, peerConnection);
         });
+        let ready = false;
+        const checkReadyState = () => {
+          let allReady = true;
+          ids.forEach((id): void => {
+            if (!this.#peers.has(id) && id !== this.#self.id) {
+              allReady = false;
+            }
+          });
+          if (allReady && !ready) {
+            log("READY", this.#self.id);
+            this.emit("ready", this.#self.id);
+            ready = true;
+          }
+          if (!ready) {
+            requestAnimationFrame(checkReadyState);
+          }
+        };
+        requestAnimationFrame(checkReadyState);
       });
     });
 
     this.#self.on("connection", (conn): void => {
-      this.subscribePeer(conn.peer, conn);
+      log("NEW_CONNECTION", conn.peer);
+      this.#subscribePeer(conn.peer, conn);
     });
   }
 
-  sendPacket(data: any): void {
-    this.#peers.forEach((conn): void => {
-      conn.send(data);
-    });
-  }
-
-  subscribePeer(id: string, conn: DataConnection): void {
-    console.log(`connecting to ${id}`);
+  #subscribePeer(id: string, conn: DataConnection): void {
     conn.on("open", (): void => {
       this.#peers.set(id, conn);
-      if (Object.values(this.#peers).every((x) => x.open))
-        this.emit("ready", this.#self.id);
+      this.emit("peerConnect", id);
     });
-    conn.on("data", (data: unknown): void => {
-      this.emit("packet", data);
+    conn.on("data", (rawData: any): void => {
+      let data: unknown;
+      if (rawData.startsWith("json:")) {
+        data = JSON.parse(rawData.slice(5));
+      } else if (rawData.startsWith("string:")) {
+        data = rawData.slice(7);
+      } else {
+        data = rawData;
+      }
+      log("INCOMING", id, data);
+      this.emit("data", data);
     });
     conn.on("close", (): void => {
+      log("CONNECTION_CLOSED", id);
       this.#peers.delete(id);
+      this.emit("peerDisconnect", id);
     });
+  }
+
+  send(rawData: any, peers?: string | string[]): void {
+    let data: string;
+    try {
+      data = `json:${JSON.stringify(rawData)}`;
+    } catch {
+      data = `string:${rawData.toString()}`;
+    }
+    log("OUTGOING", this.id, rawData);
+    if (peers) {
+      if (typeof peers === "string") this.#peers.get(peers)?.send(data);
+      else peers.forEach((id): void => this.#peers.get(id)?.send(data));
+    } else {
+      this.#peers.forEach((conn): void => {
+        conn.send(data);
+      });
+    }
   }
 
   get id(): string {
     return this.#self.id;
   }
+}
+
+function log(tag: string, id: string, rawData?: any) {
+  let data;
+  try {
+    data = JSON.parse(rawData);
+  } catch {
+    data = rawData;
+  }
+  console.log(
+    `%c[${tag}] %c${id}`,
+    "color: magenta; font-weight: bold;",
+    "color: gray;",
+    `${data ? "\n" : ""}`,
+    data ?? ""
+  );
 }
